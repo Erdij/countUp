2f33eeefd02bfa4b5b40a24e54a6f71e56d15f8e
Erdij <yildirimsukruerdi@gmail.com>
2022-11-23 19:31:01 +0300

    IconsTest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Count from "./packages/Count";
 import React from "react";
-
+import Icons from "./packages/Icons";
 function App() {
   return (
     <div className="App">
+      <Icons />
       <Count />
     </div>
   );
---
+++ src/packages/Icons.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaBeer, FaAmbulance } from "react-icons/fa";
+
+function Icons() {
+  return (
+    <div>
+      <FaBeer size="60px" color="red" /> <tr />
+      <FaAmbulance size="60px" color="red" />
+    </div>
+  );
+}
+
+export default Icons;
